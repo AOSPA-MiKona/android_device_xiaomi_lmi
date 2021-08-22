53cfcc64be5e03a574fc01b6bf97464e2843e861
Mesquita <mesquita@aospa.co>
2021-08-22 14:06:50 -0300

    Revert "[DNM] lmi: Bring back display configs"
    This reverts commit 2faf84cfab0e8c1b2c434a61a0a5fb459ea5789a.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -13,13 +13,6 @@ BUILD_BROKEN_DUP_RULES := true
 
 # Display
 TARGET_SCREEN_DENSITY := 440
-TARGET_USES_COLOR_METADATA := true
-TARGET_USES_DISPLAY_RENDER_INTENTS := true
-TARGET_USES_DRM_PP := true
-TARGET_USES_GRALLOC1 := true
-TARGET_USES_GRALLOC4 := true
-TARGET_USES_HWC2 := true
-TARGET_USES_ION := true
 
 # Fingerprint
 SOONG_CONFIG_XIAOMI_KONA_FOD_POS_X = 439
@@ -28,9 +21,6 @@ SOONG_CONFIG_XIAOMI_KONA_FOD_SIZE = 202
 
 # Kernel
 KERNEL_DEFCONFIG := lmi_defconfig
-
-# Recovery
-TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
 
 # Inherit from the proprietary version
 include vendor/xiaomi/lmi/BoardConfigVendor.mk
